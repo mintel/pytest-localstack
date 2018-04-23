2a34e5a250c5faa52ce31316e21e3efeb6dd5142
Jaye Doepke <jdoepke@mintel.com>
2018-04-23 16:41:52 -0500

    Bump version: 0.1.1 → 0.1.1.post1

--- pytest_localstack/_version.py
+++ pytest_localstack/_version.py
@@ -1,6 +1,6 @@
 # noqa
 __title__ = 'pytest-localstack'
-__version__ = '0.1.1'
+__version__ = '0.1.1.post1'
 __summary__ = 'Pytest plugin for AWS integration tests'
 __author__ = 'Jaye Doepke'
 __author_email__ = 'jdoepke@mintel.com'
